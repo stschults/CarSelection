6a160cf95562a338cd39b502242cc7618944a587
stschults <stschults@yandex.ru>
2023-08-04 14:45:13 +0300

    Added Realm Object model

--- CarSelection/Model/Models.swift
+++ CarSelection/Model/Models.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import Realm
+import RealmSwift
+
+class Car: Object {
+    @Persisted var brand: String
+    @Persisted var model: String
+    @Persisted var price: Int
+    @Persisted var state: State
+    @Persisted var mileage: Int
+    @Persisted var condition: Condition
+    @Persisted var maxPassengers: Int
+    @Persisted var maxSpeed: Int
+    @Persisted var gearBoxType: Gearbox
+    @Persisted var yearOfManufacture: Int
+}
+
+enum State: String, PersistableEnum {
+    case new
+    case used
+}
+
+enum Condition: String, PersistableEnum {
+    case perfect
+    case normal
+    case damaged
+}
+
+enum Gearbox: String, PersistableEnum {
+    case automatic
+    case mechanic
+    case robotic
+    case variatic
+}
